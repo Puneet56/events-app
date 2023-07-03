2313f82cac53e9440c6f45b5ad733be8503d7d81
= <puneet@Puneets-MacBook-Air.local>
2023-07-03 13:29:25 +0530

    Added blur view

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -1,5 +1,6 @@
+import { BlurView } from "@react-native-community/blur";
 import { useState } from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ImageBackground, ScrollView, StyleSheet, View } from "react-native";
 import {
 	Button,
 	IconButton,
@@ -33,6 +34,9 @@ const MyApp = () => {
 				width: "100%",
 				height: "100%",
 			}}
+			contentContainerStyle={{
+				alignItems: "center",
+			}}
 		>
 			<View style={styles.headerContainer}>
 				<IconButton
@@ -54,7 +58,7 @@ const MyApp = () => {
 				value={searchQuery}
 				onChangeText={setSearchQuery}
 				style={{
-					marginHorizontal: 24,
+					width: "90%",
 				}}
 				onSubmitEditing={() => {}}
 			/>
@@ -65,6 +69,7 @@ const MyApp = () => {
 					justifyContent: "space-between",
 					flexDirection: "row",
 					alignItems: "center",
+					width: "90%",
 				}}
 			>
 				<Text variant="labelLarge">Upcoming events</Text>
@@ -110,7 +115,42 @@ const MyApp = () => {
 				</Button>
 			</ScrollView>
 
-			{/* events card */}
+			<View
+				style={{
+					width: "80%",
+					alignItems: "center",
+					overflow: "hidden",
+					borderRadius: 32,
+				}}
+			>
+				<ImageBackground
+					style={{
+						width: "100%",
+						height: 300,
+						position: "relative",
+					}}
+					resizeMode="cover"
+					source={{
+						uri: "https://picsum.photos/300/200",
+					}}
+				>
+					<BlurView
+						blurType="light"
+						style={{
+							position: "absolute",
+							bottom: 0,
+							left: 0,
+							right: 0,
+							height: 70,
+							paddingHorizontal: 12,
+							paddingVertical: 4,
+							display: "flex",
+						}}
+					>
+						<Text variant="titleLarge">Hello</Text>
+					</BlurView>
+				</ImageBackground>
+			</View>
 		</ScrollView>
 	);
 };
